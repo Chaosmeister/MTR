ccbadeec6172f2ade40c7d3a5168b004dc49a6e3
Tomas Dittmann <chaosmeist3r@gmail.com>
2024-06-14 14:41:27 +0200

    add tooltip

--- Template/MoveAllToRelease.php
+++ Template/MoveAllToRelease.php
@@ -1,3 +1,3 @@
 <?php if ($column['id'] == 4) : ?>
-    <?= $this->url->icon('files-o', '', 'MtrController', 'moveall', array('column' => $column, 'project_id' => $column['project_id'], 'plugin' => 'MTR')) ?>
+    <?= $this->url->icon('files-o', '', 'MtrController', 'moveall', array('column' => $column, 'project_id' => $column['project_id'], 'plugin' => 'MTR'),false,'', 'Move all tasks to Release' ) ?>
 <?php endif ?>
