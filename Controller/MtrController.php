f566ad317137c61fe45950b7df8ac5674266d92a
Chaosmeister <13346344+Chaosmeister@users.noreply.github.com>
2024-08-16 14:39:10 +0200

    use less variables

--- Controller/MtrController.php
+++ Controller/MtrController.php
@@ -7,33 +7,46 @@ use Kanboard\Controller\BaseController;
 
 class MtrController extends BaseController
 {
+    const SourceProjectId = 1;
     const TargetProjectId = 11;
+    const Column = 4;
 
     public function move()
     {
-        $task = $this->getTask();
-        $currentproject = $task['project_id'];
+        if (!$this->projectPermissionModel->isUserAllowed(self::SourceProjectId, $this->userSession->getId())) {
+            throw new AccessForbiddenException();
+        }
 
         if (!$this->projectPermissionModel->isUserAllowed(self::TargetProjectId, $this->userSession->getId())) {
             throw new AccessForbiddenException();
         }
 
+        $task = $this->getTask();
+
         if ($this->taskProjectMoveModel->moveToProject($task['id'], self::TargetProjectId)) {
-            return $this->response->redirect($this->helper->url->to('BoardViewController', 'show', array('project_id' => $currentproject)));
+            return $this->response->redirect($this->helper->url->to('BoardViewController', 'show', array('project_id' => $task['project_id'])));
         }
     }
 
     public function moveall()
     {
-        $tasks = $this->taskFinderModel->getAll(1);
+        if (!$this->projectPermissionModel->isUserAllowed(self::SourceProjectId, $this->userSession->getId())) {
+            throw new AccessForbiddenException();
+        }
+
+        if (!$this->projectPermissionModel->isUserAllowed(self::TargetProjectId, $this->userSession->getId())) {
+            throw new AccessForbiddenException();
+        }
+
+        $tasks = $this->taskFinderModel->getAll(self::SourceProjectId);
 
         foreach ($tasks as $task) {
-            if ($task['column_id'] == 4) {
+            if ($task['column_id'] == self::Column) {
                 if ($task['id'] != 482)
                 $this->taskProjectMoveModel->moveToProject($task['id'], self::TargetProjectId);
             }
         }
 
-        return $this->response->redirect($this->helper->url->to('BoardViewController', 'show', array('project_id' => '1')));
+        return $this->response->redirect($this->helper->url->to('BoardViewController', 'show', array('project_id' => self::SourceProjectId)));
     }
 }
--- Template/MoveAllToRelease.php
+++ Template/MoveAllToRelease.php
@@ -1,3 +1,3 @@
 <?php if ($column['id'] == 4) : ?>
-    <?= $this->url->icon('files-o', '', 'MtrController', 'moveall', array('column' => $column, 'project_id' => $column['project_id'], 'plugin' => 'MTR'),false,'', 'Move all tasks to Release' ) ?>
+    <?= $this->url->icon('files-o', '', 'MtrController', 'moveall', array('plugin' => 'MTR'),false,'', 'Move all tasks to Release' ) ?>
 <?php endif ?>
